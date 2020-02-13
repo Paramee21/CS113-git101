140d889590a63084f7bc4b1009313053d66b379e
Paramee <paramee.ra@ku.th>
2020-02-13 18:53:00 +0700

    .Add week1.c

---
+++ week2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+char name[20];
+char number[5];
+
+int main()
+{
+    //printf("%s\n",name);
+    //int i;
+    //scanf("%s %d",name,&i);
+    //printf("Enter name :");
+    //gets(name);
+    //fgets(name, 20,stdin);
+    //printf("name = %s\n",name);
+    //char number[5];
+    //fgets(number, 5, stdin); //เก็บ\0,enterด้วย
+    //int i = atoi(number); //atoi=array to integer ถ้าใส่ตัวอักษรไป i=0
+    //printf("number = %s, i = %d\n", number, i);
+    //char a = getchar(); //รับinput
+    //getchar();
+    //char b = getchar();
+    //long l = atoi(number);
+    //double d = atof(number);
+    //puts(number); //รับแค่สตริง
+    //putchar(100); putchar('a');
+
+    // int found = 0; //false
+    // float isExist = 0.0; //false
+    // char isOnline = '\0'; //null character = false
+
+    // fgets(number, 5, stdin); 
+    // int i = atoi(number);
+
+    // if (i>=0 && i<=10){
+    //     printf("if = %d", i>=0 && i<=10 ); //if True return 1
+    // } else if (i <= 50 || !found){
+    //     printf("Else if");
+    // }else{
+    //     printf("else");
+    // }
+
+     fgets(number, 5, stdin); 
+     int i = atoi(number);
+    // int count = 0;
+    // while (count < i){
+    //     //count++ * count - 5 ;
+    //     //++count * count - 5 ; //++countทำก่อนcount++
+    //     printf("%d\n",count);
+    //     //i++; //interupt infinite loop = ctrl+c
+    //     count++;
+    // }
+
+    // for (int j = 0 ; 1; j = 10){ //เริ่ม เงื่อนไข จบ
+    //     printf("%d\n", j);
+    // }
+    
+    // while (1){ //infinite loop
+
+    // } 
+    // 0.1 != 10.0/100.0 << (10%)
+}
